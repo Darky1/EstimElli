55f2f23a42a62c6c00e0735de23c1c9dc9e9e765
Vadim Slivinski <wandrsl@gmail.com>
2018-10-17 23:53:34 +0300

    Mapped a plan for a getProjMat function

---
+++ getProjMat.m
@@ -0,0 +1,12 @@
+function [ projMat ] = getProjMat( vec1, vec2 )
+%getProjMat returns transformation (projection) matrix so that projMat * x
+%is a projection of x onto vec1, vec2 plane. The assumption is that vec1 and
+%vec2 are vector columns and are of the same size
+
+% get basis
+% g-s ortogonalization
+% now tranform basis vectors and group them in column
+%return
+
+end
+
